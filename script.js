6ad18ff9e30be83db23a0879a5c31fb1b7b059cb
Karun Binny <95923091+lykanranger@users.noreply.github.com>
2025-09-13 21:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,44 +6,45 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-
 getMovies(API_URL)
 
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-
     showMovies(data.results)
 }
 
 function showMovies(movies) {
     main.innerHTML = ''
-
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
-
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
-
         movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
-            <div class="overview">
-          <h3>Overview</h3>
-          ${overview}
-        </div>
         `
+        movieEl.addEventListener('click', () => {
+            const movieData = {
+                title,
+                poster: IMG_PATH + poster_path,
+                rating: vote_average,
+                overview
+            }
+            localStorage.setItem('selectedMovie', JSON.stringify(movieData))
+            window.location.href = 'movie.html'
+        })
         main.appendChild(movieEl)
     })
 }
 
 function getClassByRate(vote) {
-    if(vote >= 8) {
+    if (vote >= 8) {
         return 'green'
-    } else if(vote >= 5) {
+    } else if (vote >= 5) {
         return 'orange'
     } else {
         return 'red'
@@ -52,12 +53,9 @@ function getClassByRate(vote) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
     const searchTerm = search.value
-
-    if(searchTerm && searchTerm !== '') {
+    if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
-
         search.value = ''
     } else {
         window.location.reload()
